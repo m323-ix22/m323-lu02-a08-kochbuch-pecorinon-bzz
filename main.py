3c563b642068d3aa33e3eceaf2216ea0e2686cad
pecorinon-bzz <pecorinon@bzz.ch>
2024-09-02 09:25:52 +0200

    commit

--- main.py
+++ main.py
@@ -42,7 +42,6 @@ def load_recipe(json_string):
 
 
 if __name__ == '__main__':
-    
     # Beispiel für die Datenstruktur eines Rezepts
     recipe_json_str = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, ' \
                       '"Minced Meat": 500}, "servings": 4} '
